e06889c4361d111d74c3875ea79b3d335a3611b8
Liam <lmcardle@uwaterloo.ca>
2022-09-12 19:50:46 -0400

    Flood Fill Submission

---
+++ FloodFill/floodfill.py
@@ -0,0 +1,23 @@
+class Solution:
+    def floodFill(self, image: List[List[int]], sr: int, sc: int, color: int) -> List[List[int]]:
+        if(image[sr][sc] == color):
+            return image
+        
+        old = image[sr][sc]
+        stack = [(sr,sc)]
+        
+        while(stack):
+            i,j = stack.pop()
+            if(image[i][j] == old):
+                image[i][j] = color    
+                if(i + 1 < len(image)):
+                    stack.append((i + 1, j))
+                if(i - 1 >= 0):
+                    stack.append((i - 1, j))
+                if(j + 1 < len(image[0])):
+                    stack.append((i, j + 1))
+                if(j - 1 >= 0):
+                    stack.append((i, j - 1))
+            else:
+                pass
+        return image
